e85f0eef599a3820070e8774e4fa1b664027b0d1
iv4n999 <158744825+iv4n999@users.noreply.github.com>
2026-01-13 12:59:26 +0300

    Update bask-server.js

--- test/SEO/bask-server.js
+++ test/SEO/bask-server.js
@@ -1,1 +1,31 @@
+app.get('/services', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/services.html'));
+});
 
+app.get('/services.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/services.html'));
+});
+
+app.get('/prices', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/prices.html'));
+});
+
+app.get('/prices.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/prices.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/about.html'));
+});
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/about.html'));
+});
+
+app.get('/contacts', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/contacts.html'));
+});
+
+app.get('/contacts.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/contacts.html'));
+});
